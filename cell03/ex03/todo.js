6ce0d0bf17532a2f627591a161c4602bfad6bd6a
Thomas Da Silva <thomas@MacBook-Pro-de-Thomas.local>
2022-04-30 14:53:43 +0200

    Samedi 30 avril 2022 - push 6 -

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,50 +1,103 @@
-var global = document.getElementById('global');
+var global = document.getElementById("global");
 var findStyleGlobal = getComputedStyle(global);
 
-var ft_list = document.getElementById('ft_list');
+var ft_list = document.getElementById("ft_list");
 var findStyleList = getComputedStyle(ft_list);
 
-var task = document.querySelector('.task');
-var butonNew = document.getElementById('buttonNew');
+var task = document.querySelector(".task");
+var butonNew = document.getElementById("buttonNew");
 
-var taskclass = document.getElementsByClassName('task');
+var taskclass = document.getElementsByClassName("task");
+var j = 0;
 
-document.cookie = 'stockDiv; max-age = 30*24*60*60';
-var c = document.cookie;
+function stockNumChr()
+{
+	if (document.cookie)
+	{
+		for (var i = 0 ; document.cookie.split(";")[i]; i++)
+		{
+			if (document.cookie.split(";")[i].split("=")[0] == "stockNum" || document.cookie.split(";")[i].split("=")[0] == " stockNum")
+			{
+				return (document.cookie.split(";")[i].split("=")[1]);
+			}	
+		}
+		return (0);
+	}
+	else
+		return (0);
+}
 
-console.log(c);
+if (!stockNumChr())
+{
+	console.log("Creat Cookies stockNum");
+	document.cookie = "stockNum" + "=" + 1 + "; expires = 01 Jan 3000 00:00:00 UTC";
+}
 
-function deleteTask() {
-	if (confirm('Delete ?'))
+function addCookie(idOfDiv, content) 
+{
+	document.cookie = idOfDiv + "=" + content + "; expires = 01 Jan 3000 00:00:00 UTC";
+}
+
+for (0 ; document.cookie.split(";")[j]; j++)
+{
+	if (document.cookie.split(";")[j].split("=")[0] == "stockNum" || document.cookie.split(";")[j].split("=")[0] == " stockNum")
+	{
+		console.log("Cookie stockNum = "+stockNumChr());
+	}
+	else
+	{
+		var newDiv = document.createElement("div");
+		newDiv.className = "task";
+		newDiv.id = document.cookie.split(";")[j].split("=")[0];
+		newDiv.onclick = deleteTask;
+		var newDivTxt = document.createElement("h6");
+		newDiv.appendChild(newDivTxt);
+		newDivTxt.innerHTML = document.cookie.split(";")[j].split("=")[1];
+		ft_list.prepend(newDiv);
+	}
+}
+if (j > 1) 
+{
+	global.style.height = parseInt(findStyleGlobal.height.replace(/px/,"")) + (88 * (j - 2)) + "px";
+	ft_list.style.height = parseInt(findStyleList.height.replace(/px/,"")) + (88 * (j - 2)) + "px";
+}
+
+function deleteTask() 
+{
+	if (confirm("Delete ?"))
 	{
 		console.log("Delete this div");
 		ft_list.removeChild(this);
 		if (ft_list.childNodes.length > 1) 
 		{
-			global.style.height = parseInt(findStyleGlobal.height.replace(/px/,""))-88+"px";
-			ft_list.style.height = parseInt(findStyleList.height.replace(/px/,""))-88+"px";
+			global.style.height = parseInt(findStyleGlobal.height.replace(/px/,"")) - 88 + "px";
+			ft_list.style.height = parseInt(findStyleList.height.replace(/px/,"")) - 88 + "px";
 		}
+		document.cookie = this.id + "=" + this + "; expires = Thu, 01 Jan 1970 00:00:00 UTC";
 	}
 }
 
-butonNew.addEventListener('click', function() {
+butonNew.addEventListener("click", function() 
+{
 	let txt = prompt();
 	if (txt)
 	{
 		if (ft_list.childNodes.length > 1) 
 		{
-			global.style.height = parseInt(findStyleGlobal.height.replace(/px/,""))+88+"px";
-			ft_list.style.height = parseInt(findStyleList.height.replace(/px/,""))+88+"px";
+			global.style.height = parseInt(findStyleGlobal.height.replace(/px/,"")) + 88 + "px";
+			ft_list.style.height = parseInt(findStyleList.height.replace(/px/,"")) + 88 + "px";
 		}
 
-		console.log('Click on button New');
-		var newDiv = document.createElement('div');
+		console.log("Click on button New");
+		var newDiv = document.createElement("div");
 		newDiv.className = "task";
+		newDiv.id = parseInt(stockNumChr().replace(/ /,""));
 		newDiv.onclick = deleteTask;
-		var newDivTxt = document.createElement('h6');
+		var newDivTxt = document.createElement("h6");
 		newDiv.appendChild(newDivTxt);
-		newDivTxt.innerHTML=txt;
+		newDivTxt.innerHTML = txt;
+		addCookie(parseInt(stockNumChr().replace(/ /,"")), txt);
 		ft_list.prepend(newDiv);
+		document.cookie = "stockNum" + "=" + (parseInt(stockNumChr().replace(/ /,"")) + 1) + "; expires = 01 Jan 3000 00:00:00 UTC";
 	}
 })
-
